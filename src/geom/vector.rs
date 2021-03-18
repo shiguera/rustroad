fcbc9e4519748dd8ac5965e9736685156ff00c4a
shiguera <shiguera@mercatorlab.com>
2021-03-18 22:58:28 +0100

    Add geom::vector::Vector::unit_vector() and geom::vector::Vector::bearing()
    bearing() method makes comparisons with ==; This is not accurate, and must be solved

--- src/geom/vector.rs
+++ src/geom/vector.rs
@@ -1,3 +1,4 @@
+use std::f64::consts::PI;
 // 2 D vector
 pub struct Vector {
    pub x: f64,
@@ -7,6 +8,54 @@ pub struct Vector {
 impl Vector {
    pub fn new(x: f64, y: f64) -> Self {
       Vector{x,y}
+   }
+   pub fn length(&self) -> f64 {
+      ((self.x*self.x)+(self.y*self.y)).sqrt()
+   }
+   pub fn unit_vector(&self) -> Self {
+      let module = self.length();
+      Vector::new(self.x/module, self.y/module)
+   }
+   pub fn bearing(&self) -> f64 {
+      // Angle with X axis in radians
+      // East==Positive X axis is the origin of angles
+      // Counterclockwise is the direction 
+      let mut result:f64 = 0.0;
+      if self.x==0.0 && self.y==0.0 {
+         result = 0.0_f64;         
+      }
+      if self.x == 0.0 {
+         if self.y > 0.0 {
+            result=PI/2.0;
+         } else {
+            result = 3.0*PI/2.0;
+         } 
+      } 
+      if self.y == 0.0 {
+         if self.x < 0.0 {
+            result = PI;
+         } else {
+            result = 0.0;
+         }
+      } 
+      let tangent = self.y/self.x;
+      let angle = tangent.atan().abs();
+      
+      if self.x < 0.0 {
+         if self.y < 0.0 {
+            result = angle + PI;
+         } else {
+            result = PI - angle;
+         }
+      }
+      if self.x>0.0 { 
+         if self.y<0.0 {
+            result = 2.0*PI-angle;
+         } else {
+            result = angle;
+         }
+      }
+      result
    }
 }
 
@@ -19,5 +68,52 @@ mod tests {
       let v = Vector::new(1.0,-1.0);
       assert_eq!(true, v.x-1.0 < 0.001);
       assert_eq!(true, v.y-(-1.0) < 0.001);
+   }
+   #[test]
+   fn test_length() {
+      let v = Vector::new(1.0,-1.0);
+      assert_eq!(true, v.length()-2.0_f64.sqrt() < 0.001);
+      let v = Vector::new(0.0,0.0);
+      assert_eq!(true, v.length()-0.0_f64 < 0.001);
+      
+   }
+   #[test]
+   fn test_unit_vector() {
+      let v = Vector::new(1.0,-1.0);
+      assert_eq!(true, v.unit_vector().length()-1.0_f64 < 0.001);
+      assert_eq!(true, v.unit_vector().x - 0.7071 < 0.0001);
+      assert_eq!(true, v.unit_vector().y - (-0.7071) < 0.0001);      
+   }
+   #[test]
+   fn test_bearing() {
+      let v = Vector::new(0.0, 0.0);
+      assert_eq!(true, v.bearing() == 0.0);
+      let v = Vector::new(1.0, 0.0);
+      assert_eq!(true, v.bearing() == 0.0);
+      let v = Vector::new(-1.0, 0.0);
+      assert_eq!(true, v.bearing() == PI);
+      let v = Vector::new(0.0, 1.0);
+      assert_eq!(true, v.bearing() == PI/2.0);
+      let v = Vector::new(0.0, -1.0);
+      assert_eq!(true, v.bearing() == 3.0*PI/2.0);
+      let v = Vector::new(1.0, 1.0);
+      assert_eq!(true, v.bearing() == PI/4.0);
+      let v = Vector::new(1.0, -1.0);
+      assert_eq!(true, v.bearing() == 2.0*PI - PI/4.0);
+      let v = Vector::new(-1.0, 1.0);
+      assert_eq!(true, v.bearing() == PI/2.0 + PI/4.0);
+      let v = Vector::new(-1.0, -1.0);
+      println!("{} {}", v.bearing(), PI + PI/4.0);
+      assert_eq!(true, v.bearing() == PI + PI/4.0);
+ 
+   }
+   #[test]
+   fn test_1() {
+      let a: f64 = 0.15 + 0.15 + 0.15;
+      let b: f64 = 0.1 + 0.1 + 0.25;
+      println!("{} == {}", a, b);
+      println!("{}", a==b);  // Fails, because they are not exactly equal
+      println!("{}", f64::MIN);
+      println!("{}", f64::EPSILON);
    }
 }
