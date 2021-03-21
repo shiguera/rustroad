3ff8785770cd6e54b24ebe5257dfab819b2f9a67
shiguera <shiguera@mercatorlab.com>
2021-03-21 19:46:03 +0100

    Implement lib::factorial() function

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,13 @@ mod road;
 pub fn eq(x:f64, y:f64) -> bool {
    approx_eq!(f64, x, y, ulps=2)
 }
+fn factorial(num: u64) -> u64 {
+   match num {
+       0 => 1,
+       1 => 1,
+       _ => factorial(num - 1) * num,
+   }
+}
 
 #[cfg(test)]
 mod tests {
@@ -20,5 +27,17 @@ mod tests {
       let y = 0.20+0.20+0.20;
       println!("x={} y={} x==y {}", x, y, x==y);
       assert_eq!(true, eq(0.15+0.15+0.15+0.15, 0.20+0.20+0.20));
+    }
+
+    #[test]
+    fn test_factorial() {
+       let n = 0;
+       assert_eq!(true, factorial(n)==1);
+       let n = 1;
+       assert_eq!(true, factorial(n)==1);
+       let n = 5;
+       assert_eq!(true, factorial(n)==120);
+       let x = factorial(5) as f64;
+       println!("{}", x*2.0);
     }
 }
