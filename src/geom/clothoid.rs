aae1ff511ef8406b71d3a2876c5c1f02c909b7ac
shiguera <shiguera@mercatorlab.com>
2021-03-23 12:50:02 +0100

    Second version of clothoid as generic from origin (0,0)

--- src/geom/clothoid.rs
+++ src/geom/clothoid.rs
@@ -2,35 +2,24 @@ use crate::*;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Clothoid {
-   // Clothoid.
-   // Assumes one of the radius is zero (infinity)
-   // It uses zero for infinity radius
-   pub length: f64,
-   pub start_radius: f64, 
+   // Generic clothoid starting in the origin (0, 0) 
+   // with tangent horizontal and infinity radius in this point.
+   // It uses zero as radius value for infinity radius
+   // There are 4 possible solutions. If parameter A is positive, the solution is
+   // in the first or fourth quadrant. If parameter A is negative, the solution
+   // is in the second or thirth quadrant
+   // If ending radius is positive, clothoid is counterclockwise (1st or 3th quadrant) 
+   // If ending radius is negative, the clothoid is clockwise (4th or 2nd quadrant)
+   // start_radius is always zero (infinity)
+   // length is obtained from L = A^2/end_radius
+   pub parameter: f64, 
    pub end_radius: f64 // radius>0 => counterclockwise
 }
 
 impl Clothoid {
-   pub fn new(length: f64, start_radius: f64, end_radius: f64) -> Self {
-      if eq(start_radius, 0.0) && eq(end_radius, 0.0) {
-         // The two radius can't be zero
-         panic!("Clothoid with zero radius in the two extremes")
-      }
-      if !eq(start_radius, 0.0) && !eq(end_radius, 0.0) {
-         // One of the radius must be zero
-         panic!("Clothoid without zero radius in any extreme")
-      }    
-      Clothoid{length, start_radius, end_radius}
+   pub fn new(parameter: f64, end_radius: f64) -> Self {
+      Clothoid{parameter, end_radius}
    }
-   pub fn parameter(self) -> f64 {
-      // Parameter is A with L*R=A^2
-      if eq(self.start_radius, 0.0) {
-         (self.length*self.end_radius.abs()).sqrt()
-      } else {
-         (self.length*self.start_radius.abs()).sqrt()
-      }
-   }
-   
 }
 
 mod tests {
@@ -39,40 +28,17 @@ mod tests {
 
    #[test]
    fn test_new() {
-      let c = Clothoid::new(100.0, 0.0, 400.0);
-      assert_eq!(true, eq(100.0, c.length));
-      assert_eq!(true, eq(0.0, c.start_radius));
+      let c = Clothoid::new(500.0, 400.0);
+      assert_eq!(true, eq(500.0, c.parameter));
       assert_eq!(true, eq(400.0, c.end_radius));      
-   }
-   #[test]
-   #[should_panic]
-   fn test_new_panic() {
-      // One of the radius must be zero
-      let _c = Clothoid::new(100.0, 200.0, 400.0);
-   }
-   #[test]
-   #[should_panic]
-   fn test_new_panic_2() {
-      // The two radius can't be zero
-      let _c = Clothoid::new(100.0, 0.0, 0.0);
-   }
-   #[test]
-   fn test_parameter() {
-      let c = Clothoid::new(100.0, 0.0, 400.0);
-      assert_eq!(true, eq(40000.0_f64.sqrt(), c.parameter()));
-      let c = Clothoid::new(100.0, 400.0, 0.0);
-      assert_eq!(true, eq(40000.0_f64.sqrt(), c.parameter()));
-      let c = Clothoid::new(100.0, -400.0, 0.0);
-      assert_eq!(true, eq(40000.0_f64.sqrt(), c.parameter()));
-      let c = Clothoid::new(100.0, 0.0, -400.0);
-      assert_eq!(true, eq(40000.0_f64.sqrt(), c.parameter()));
-      
    }
    #[test]
    fn test_1() {
       let start_radius = 0.0;
       let end_radius = 100.0;
       println!("{} {} {}", !eq(start_radius, 0.0), !eq(end_radius, 0.0), !eq(start_radius, 0.0) || !eq(end_radius, 0.0));
-
+      let x = 2.0_f64;
+      let y = x.powi(3);
+      println!("{}",y);
    }
 }
