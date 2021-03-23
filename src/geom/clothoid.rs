a077258ca84be7715e3e9a4616ff8571e775b8a6
shiguera <shiguera@mercatorlab.com>
2021-03-23 14:02:56 +0100

    Add some methods for geom::clothid::Clothoid
    Add length(), azimuth_increment() and end_azimuth()
    methods for geom::clothoid::Clothoid

--- src/geom/clothoid.rs
+++ src/geom/clothoid.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use std::f64::consts::PI;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Clothoid {
@@ -18,7 +19,32 @@ pub struct Clothoid {
 
 impl Clothoid {
    pub fn new(parameter: f64, end_radius: f64) -> Self {
+      if eq(end_radius, 0.0_f64) {
+         panic!("Clothoid creation error: end_radius can't be zero");
+      }
+      if eq(parameter, 0.0_f64) {
+         panic!("Clothoid creation error: parameter can't be zero");
+      }
+      
       Clothoid{parameter, end_radius}
+   }
+   pub fn length(self) -> f64 {
+      self.parameter*self.parameter / self.end_radius.abs()
+   }
+   pub fn end_azimuth(self) -> f64 {
+      let alpha_l = self.azimuth_increment();
+      if self.parameter > 0.0 {
+         if self.end_radius > 0.0 {
+               alpha_l
+         } else {
+               2.0*PI-alpha_l.abs()
+         }
+      }  else {
+            PI+alpha_l
+      }    
+   }  
+   pub fn azimuth_increment(self) -> f64 {
+      self.length() / self.end_radius / 2.0
    }
 }
 
@@ -28,9 +54,50 @@ mod tests {
 
    #[test]
    fn test_new() {
-      let c = Clothoid::new(500.0, 400.0);
-      assert_eq!(true, eq(500.0, c.parameter));
-      assert_eq!(true, eq(400.0, c.end_radius));      
+      let c = Clothoid::new(190.0, 450.0);
+      assert_eq!(true, eq(190.0, c.parameter));
+      assert_eq!(true, eq(450.0, c.end_radius));      
+   }
+   #[test]
+   #[should_panic]
+   fn test_new_panic_1() {
+      let _c = Clothoid::new(0.0, 400.0);
+   }
+   #[test]
+   #[should_panic]
+   fn test_new_panic_2() {
+      let _c = Clothoid::new(1000.0, 0.0);
+   }
+
+   #[test]
+   fn test_length() {
+      let c = Clothoid::new(190.0, 450.0);
+      assert_eq!(true, (c.length()-80.222).abs()<0.001);
+      let c = Clothoid::new(190.0, -450.0);
+      assert_eq!(true, (c.length()-80.222).abs()<0.001);
+      let c = Clothoid::new(-190.0, 450.0);
+      assert_eq!(true, (c.length()-80.222).abs()<0.001);
+      let c = Clothoid::new(-190.0, -450.0);
+      assert_eq!(true, (c.length()-80.222).abs()<0.001);
+   }
+   #[test]
+   fn test_alpha_l() {
+      let c = Clothoid::new(190.0, 450.0);
+      assert_eq!(true, (c.azimuth_increment()-0.08914).abs()<0.001);
+      let c = Clothoid::new(190.0, -450.0);
+      assert_eq!(true, (c.azimuth_increment()+0.08914).abs()<0.001);
+   }
+
+   #[test]
+   fn test_end_azimuth() {
+      let c = Clothoid::new(190.0, 450.0);
+      assert_eq!(true, (c.end_azimuth()-0.08914).abs()<0.001);
+      let c = Clothoid::new(190.0, -450.0);
+      assert_eq!(true, (c.end_azimuth()-6.1940).abs()<0.001);
+      let c = Clothoid::new(-190.0, 450.0);
+      assert_eq!(true, (c.end_azimuth()-3.2307).abs()<0.001);
+      let c = Clothoid::new(-190.0, -450.0);
+      assert_eq!(true, (c.end_azimuth()-3.0525).abs()<0.001);
    }
    #[test]
    fn test_1() {
