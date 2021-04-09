c93908f430e79a30b2c97e28caea97b5caf09290
shiguera <shiguera@mercatorlab.com>
2021-04-10 01:35:00 +0200

    Add methods alpha(), x() and y() to geom::clothoid::Clothoid
    The three methods need check when parameter or radius are negative

--- src/geom/clothoid.rs
+++ src/geom/clothoid.rs
@@ -1,5 +1,8 @@
 use crate::*;
 use std::f64::consts::PI;
+use factorial::Factorial;
+
+const NUMITER:i64 = 10_i64;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Clothoid {
@@ -12,7 +15,7 @@ pub struct Clothoid {
    // If ending radius is positive, clothoid is counterclockwise (1st or 3th quadrant) 
    // If ending radius is negative, the clothoid is clockwise (4th or 2nd quadrant)
    // start_radius is always zero (infinity)
-   // length is obtained from L = A^2/end_radius
+   // length is obtained from L = abs(A^2/end_radius)
    pub parameter: f64, 
    pub end_radius: f64 // radius>0 => counterclockwise
 }
@@ -45,6 +48,24 @@ impl Clothoid {
    }  
    pub fn azimuth_increment(self) -> f64 {
       self.length() / self.end_radius / 2.0
+   }
+   pub fn alpha(&self, s:f64) -> f64 {
+      s*s / 2.0 / self.parameter.powi(2)
+   }
+   pub fn x(&self, s:f64) -> f64 {
+      let alpha = self.alpha(s);
+      let mut x = 1.0 - alpha.powi(2)/10.0 + alpha.powi(4)/9.0/24.0-alpha.powi(6)/13.0/720.0;
+      x = x * self.parameter.abs()*(2.0*alpha).sqrt();
+      x
+   }
+   pub fn y(&self, s: f64) -> f64 {
+      // Calculate y coordinate for the point 
+      // where the length of curve arc is s
+      let alpha = self.alpha(s);
+      let mut y = alpha/3.0 - alpha.powi(3)/7.0/6.0+alpha.powi(5)/11.0/120.0;
+      y = y * self.parameter.abs()*(2.0*alpha).sqrt();
+      y
+
    }
 }
 
@@ -107,5 +128,41 @@ mod tests {
       let x = 2.0_f64;
       let y = x.powi(3);
       println!("{}",y);
+   }
+   #[test]
+   fn test_alpha() {
+      let parameter = 500.0;
+      let end_radius  = 400.0;
+      let cl = Clothoid::new(parameter, end_radius);
+      let len = cl.length();
+      let endaz = cl.azimuth_increment();
+      let alpha = cl.alpha(len);
+      assert_eq!(true, eq(endaz, alpha));
+   }
+   #[test]
+   fn test_x() {
+      let parameter = 190_f64;
+      //println!("{}", parameter);
+      let end_radius = 450_f64;
+      //println!("{}", end_radius);
+      let cl = Clothoid::new(parameter, end_radius);
+      let len = cl.length();
+      println!("{}", len);
+      let x = cl.x(len);
+      println!("{}", x);
+      
+   }
+   #[test]
+   fn test_y() {
+      let parameter = 190_f64;
+      //println!("{}", parameter);
+      let end_radius = 450_f64;
+      //println!("{}", end_radius);
+      let cl = Clothoid::new(parameter, end_radius);
+      let len = cl.length();
+      println!("{}", len);
+      let y = cl.y(len);
+      println!("{}", y);
+      
    }
 }
