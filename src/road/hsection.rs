cf3e6928d9bf2c742dce1c3ec9f8775d8b23deeb
shiguera <shiguera@mercatorlab.com>
2021-04-26 14:19:38 +0200

    Add methods to trait road::hsection

--- src/road/hsection.rs
+++ src/road/hsection.rs
@@ -3,11 +3,23 @@ use crate::geom::point::Point;
 // Sections from horizontal alignment (Tangent, Spiral and CircularCurve)
 // are well defined with these five parameters
 pub trait HSection {
-   fn start_point() -> Point;
-   fn end_point() -> Point;
-   fn start_radius() -> f64;
-   fn end_radius() -> f64;
-   fn length() -> f64;
-   fn start_azimuth() -> f64;
-   fn end_azimuth() -> f64;
+   fn start_point(&self) -> Point;
+   fn start_x(&self) -> f64 {
+      self.start_point().x
+   }
+   fn start_y(&self) -> f64 {
+      self.start_point().y
+   }
+   fn end_point(&self) -> Point;
+   fn end_x(&self) -> f64 {
+      self.end_point().x
+   }
+   fn end_y(&self) -> f64 {
+      self.end_point().y
+   }
+   fn start_radius(&self) -> f64;
+   fn end_radius(&self) -> f64;
+   fn length(&self) -> f64;
+   fn start_azimuth(&self) -> f64;
+   fn end_azimuth(&self) -> f64;
 }
